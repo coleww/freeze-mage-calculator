f18078dfd7fd3d0a70560554c89d317add06a7ee
Cole Willsea <coleww@gmail.com>
2016-10-11 14:16:10 -0700

    pass empty state to smokescreen app test

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,10 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const props = {
+    deck: [],
+    hand: [],
+    graveyard: []
+  }
+  ReactDOM.render(<App {...props} />, div);
 });
