f6cd3656ba1c817ef3e2f33689ead122be3e8cd3
Cole Willsea <coleww@gmail.com>
2016-10-12 13:57:19 -0700

    add flexbox, make responsive card cost overlay

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = React.createClass({
   render() {
     return (
       <div className="card">
-        Cost: {this.props.card.cost}
+        <div className="cardCost">{this.props.card.cost}</div>
         <img
           className="cardImg"
           src={`${process.env.PUBLIC_URL}/imgs/${this.props.card.imgSrc}.png`}
